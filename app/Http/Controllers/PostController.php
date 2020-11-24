269504000a8800b2f1749f275d61061ea0827ce6
Fabio Pereira Soares <fabiotecninf@gmail.com>
2020-11-24 16:39:50 -0300

    Tipos de Chamada

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+
+    public function indexRedirect()
+    {
+        return redirect()->route('posts.index');
+    }
+
     /**
      * Display a listing of the resource.
      *
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,11 @@
 |
 */
 //sobreescrevendo nome da rota resource
+/*
 Route::resourceVerbs([
     'create' => 'cadastrar',
     'edit' => 'editar'
-]);
+]);*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -42,13 +43,32 @@ Route::delete('/users/1', 'UserController@destroy');
 Route::any('/users', 'UserController@any');
  */
 //para metodos que não são implementados por Resource criar a rota antes da rota resource
- Route::get('/posts/premium','PostController@premium');
+ //Route::get('/posts/premium','PostController@premium');
  //Route::resource('posts', 'PostController');
  //exemplo de resource customizado passando os metodos que deseja no only
  //Route::resource('posts', 'PostController')->only('index','show');
   //exemplo de resource customizado passando os metodos que não deseja no except only
-  Route::resource('posts', 'PostController')->except('index','show');
+  //Route::resource('posts', 'PostController')->except('index','show');
 
  //para sobre escrever um metodo implementado por resource criar essa nova rota após a rota de resource
  //Route::get('/posts','PostController@teste');
+
+
+Route::get('/users',function(){
+    echo 'Listagem dos usuarios da minha base';
+});
+
+Route::view('/form','form');
+
+//responsavel por gerar a saida de rotas inválidas
+Route::fallback(function(){
+    echo '<h1>Seja muito bem vido a nossa página 404.</h1>';
+});
+
+//redirecionando uma rota
+Route::redirect('/users/add',url('/form'), 301);
+
+Route::get('/posts','PostController@index')->name("posts.index");
+
+Route::get('/posts/index','PostController@indexRedirect')->name("posts.indexRedirect");
 
