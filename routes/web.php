525432eb19cee496fc522045e488f288bb350b6d
Fabio Pereira Soares <fabiotecninf@gmail.com>
2020-11-24 18:08:03 -0300

    Agrupamento de Rota

--- routes/web.php
+++ routes/web.php
@@ -107,4 +107,35 @@ Route::get('/posts/index','PostController@indexRedirect')->name("posts.indexRedi
     var_dump($route,$name,$action);
  })->name('inspect');*/
 
- Route::get('/users/1','UserController@inspect')->name('inspect');
+ //Route::get('/users/1','UserController@inspect')->name('inspect');
+/*
+ Route::prefix('admin')->group(function(){
+
+    Route::view('/form','form');
+
+ });
+
+ Route::name('admin.post.')->group(function(){
+    Route::get('/admin/posts/index','PostController@index')->name('index');
+    Route::get('/admin/posts','PostController@show')->name('show');
+ });
+
+ Route::middleware(['throttle:10,1'])->group(function(){
+     Route::view('/form','form');
+
+ });
+
+ Route::namespace('Admin')->group(function(){
+    Route::get('/users', 'UserController@index');
+ });*/
+
+
+ Route::group([
+     'namespace' => 'Admin',
+     'prefix' => 'admin',
+     'middleware' => ['throttle:10,1'],
+     'as' => 'admin.'
+ ], function(){
+    Route::resource('users', 'UserController');
+
+ });
